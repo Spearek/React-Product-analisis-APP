315bb0b8278d7738e051b25c60245b3713105f2d
Spearek <30389214+Spearek@users.noreply.github.com>
2018-11-18 17:33:48 +0100

    initial page layout

--- analysis-app/src/App.js
+++ analysis-app/src/App.js
@@ -5,7 +5,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>Product Analysis App</h1>
+        <div className="topOfLayout">
+          <div className="products">
+            <p>Choose a product:</p>
+              <ul>
+                <li>First</li>
+                <li>Second</li>
+                <li>Third</li>
+              </ul>  
+          </div>
+          <div className="details">
+            <h3>Product Title</h3>
+            <p>Product Image</p>
+            <p>Product Description</p>
+            <p>Product current price</p>
+          </div>
+        </div>
       </div>
     );
   }
