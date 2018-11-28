0f9a40c23b53f6279717986e59c3c168bcc54b80
Spearek <30389214+Spearek@users.noreply.github.com>
2018-11-28 20:48:06 +0100

    Chart component created with example chart Data

---
+++ analysis-app/src/components/Chart/Chart.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Bar, Doughnut, Line, Pie} from 'react-charjs-2';
+
+
+const chart = () => {
+
+    const chartData = {
+        labels:['Warsaw','Łódź','Kraków','Wrocław','Poznań'],
+        datasets:[{
+            label:'Population',
+            data:[
+                1702139,
+                768755,
+                755050,
+                634893,
+                570352
+            ],
+            backgroundColor:[
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)'],
+            borderWidth: 1,
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderColor: '#000'
+
+        }]
+    }
+
+    return (
+
+    <Bar
+        data={chartData}
+        options={{
+            title: {
+                display:true,
+                text:'Largest Cities in Poland',
+                fontSize:25
+            },
+            legend:{
+                display:false,
+                position:'right',
+                labels:{
+                    fontColor:'black'
+                }
+            },
+            layout:{
+                padding:{
+                    left:50,
+                    right:0,
+                    bottom:0,
+                    top:0,
+
+                }
+            },
+            tooltips:{
+                enabled:true
+            }
+        }}
+    />  
+    )
+    
+}
+
+
+export default chart;
