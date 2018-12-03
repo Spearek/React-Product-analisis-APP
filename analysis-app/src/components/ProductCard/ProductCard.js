b9aae1be40932e3120c2607830b8042fab10ba35
Spearek <30389214+Spearek@users.noreply.github.com>
2018-12-03 21:21:10 +0100

    adding a margin

--- analysis-app/src/components/ProductCard/ProductCard.js
+++ analysis-app/src/components/ProductCard/ProductCard.js
@@ -4,6 +4,8 @@ import './ProductCard.css';
 
 const product = (props) => {
 
+    let margin = Math.round((((props.salePrice - props.purchacePrice)/props.salePrice)*100) * 100) / 100;
+
     return (
         <div className='Card'>
             <h3>{props.title}</h3>
@@ -11,6 +13,7 @@ const product = (props) => {
             <p>{props.description}</p>
             <div className="Prices">
                 <p>Current purchase price: {props.purchacePrice}</p>
+                <p>Margin: {margin}%</p>
                 <p>Current sale price: {props.salePrice}</p>
             </div>
         </div>
