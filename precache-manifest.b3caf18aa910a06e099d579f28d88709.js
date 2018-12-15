7d7aafd1ecc2dccabc1675a8fe8ccec80d3b034f
Spearek <30389214+Spearek@users.noreply.github.com>
2018-12-15 18:56:00 +0100

    Updates

---
+++ precache-manifest.b3caf18aa910a06e099d579f28d88709.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "68228a61218bfa290af40346e6e4ec24",
+    "url": "/React-Product-analisis-APP/static/media/example_processor.68228a61.jpg"
+  },
+  {
+    "revision": "3f93c058505b05ed6e57d0fe38eab84a",
+    "url": "/React-Product-analisis-APP/static/media/example_motherboard.3f93c058.jpg"
+  },
+  {
+    "revision": "ce50dfbca7248ab962f0e59247753cf5",
+    "url": "/React-Product-analisis-APP/static/media/example_memory.ce50dfbc.jpg"
+  },
+  {
+    "revision": "dadc2728517e9fa1da03eaef46503fdb",
+    "url": "/React-Product-analisis-APP/static/media/example_drive.dadc2728.jpg"
+  },
+  {
+    "revision": "f1889bbfac21c83ad73a",
+    "url": "/React-Product-analisis-APP/static/js/runtime~main.f1889bbf.js"
+  },
+  {
+    "revision": "8c8be0f41fc63730ad5a",
+    "url": "/React-Product-analisis-APP/static/js/main.8c8be0f4.chunk.js"
+  },
+  {
+    "revision": "91f30a8f8305d92d7ebf",
+    "url": "/React-Product-analisis-APP/static/js/1.91f30a8f.chunk.js"
+  },
+  {
+    "revision": "8c8be0f41fc63730ad5a",
+    "url": "/React-Product-analisis-APP/static/css/main.41c4a7a9.chunk.css"
+  },
+  {
+    "revision": "ae9e7d21634a17ef62e019515703b08f",
+    "url": "/React-Product-analisis-APP/index.html"
+  }
+];
